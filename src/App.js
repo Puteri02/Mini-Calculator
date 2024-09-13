fc0dd3e420075e2bfdf5cccd9d4bf22395266cdf
pu3 <puterifiqrina02@gmail.com>
2024-09-13 15:02:33 +0800

    test

--- src/App.js
+++ src/App.js
@@ -78,6 +78,9 @@ function App() {
             <input type="button" value="3" onClick={e => setValue(value + e.target.value)} />
             <input type="button" value="-" onClick={e => setValue(value + e.target.value)} />
           </div>
+
+
+          
           <div>
             <input type="button" value="00" onClick={e => setValue(value + e.target.value)} />
             <input type="button" value="0" onClick={e => setValue(value + e.target.value)} />
